4d8e440a8e07fec7fbf299457eae96d9e7eb796e
Romanas <go.romanas@gmail.com>
2021-03-04 09:39:29 +0200

    fixed styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,20 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Blog Demo",
+    title: 'Gatsby Blog Demo',
   },
   plugins: [
     'gatsby-plugin-postcss',
+    'gatsby-plugin-styled-components',
     {
-      resolve: "gatsby-source-datocms",
+      resolve: 'gatsby-source-datocms',
       options: {
-        apiToken: 'a240e601d64b0116f05f40b2d547c3',
+        apiToken: process.env.DATO_API_TOKEN,
         environment: process.env.DATO_ENVIRONMENT,
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
   ],
-};
+}
