0b02847155e79fbd657b7e11fa4da4d4b4119ecc
Romanas <go.romanas@gmail.com>
2021-04-25 23:54:45 +0300

    analytics fiix

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,24 +37,10 @@ module.exports = {
     },
     `gatsby-plugin-transition-link`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'G-LT54PFHV9D',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'stukaite.lt',
+        id: 'GTM-M8TC2ZN',
+        includeInDevelopment: false,
       },
     },
   ],
