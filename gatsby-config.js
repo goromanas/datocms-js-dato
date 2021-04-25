a59ce72aaf5012495cdcf7ada8908adcf7f95f94
Romanas <go.romanas@gmail.com>
2021-04-25 21:08:58 +0300

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
-          displayName: false
+        displayName: false,
       },
     },
     'gatsby-plugin-antd',
@@ -36,5 +36,58 @@ module.exports = {
       },
     },
     `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        // output: `/some-other-sitemap.xml`,
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        // exclude: [`/category/*`, `/path/to/page`],
+        query: `
+        {
+          datoCmsSite {
+            globalSeo {
+              siteName
+            }
+          }
+          allDatoCmsPage {
+            edges {
+              node {
+                slug
+              }
+            }
+          }
+        }`,
+        serialize: ({ datoCmsSite, allDatoCmsPage }) =>
+          allDatoCmsPage.edges.map((item) => {
+            return {
+              url: `${datoCmsSite.globalSeo.siteName}/${item.node.slug}`,
+              priority: 0.7,
+            }
+          }),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'G-C3LH3JXHQ7',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'stukaite.lt',
+      },
+    },
   ],
 }
